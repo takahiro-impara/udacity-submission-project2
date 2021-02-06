c48c4693072bbd5ed6f41641d8c2064e2273cd1d
takahiro-impara <takahiro4120@gmail.com>
2021-02-06 14:51:54 +0900

    create auth router

--- src/controllers/v0/auth/auth.router.ts
+++ src/controllers/v0/auth/auth.router.ts
@@ -2,7 +2,52 @@ import { Router, Request, Response } from 'express';
 
 const router: Router = Router();
 
-router.get('/', async(req: Request, res: Response) => {
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User1 = {
+  name: "Taro",
+  password: "Taro123"
+};
+
+
+passport.use(new LocalStrategy(
+  (username: any, password:any, done:any) => {
+
+    if(username !== User1.name){
+      // Error
+      return done(null, false);
+    } else if(password !== User1.password) {
+      // Error
+      console.log("error");
+      return done(null, false);
+    } else {
+      // Success and return user information.
+      return done(null, { username: username, password: password});
+    }
+  }
+));
+
+router.use(passport.initialize());
+
+router.post('/login',
+  (req, res, next) => {
+    console.log(req);
+    next();
+  },
+  passport.authenticate('local',
+    {
+      session: false,
+      failureRedirect : '/api/v0/auth/login'
+    }
+  ),
+  (req, res) => {
+    console.log(req);
+    res.send('Success');
+  }
+);
+
+router.get('/login', async(req: Request, res: Response) => {
   res.sendFile(__dirname + '/auth.html');
 });
 
